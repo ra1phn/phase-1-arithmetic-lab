01b1fd1c86ac81daf88322d722456df71bcdd898
Ralph <njugunaralph@gmail.com>
2025-06-04 22:27:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2; 
+console.log(multiply); // Output: 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; // Random integer between 1 and 100
+console.log(random); // Output: Random number between 1 and 100
+
+let num3 = 28;
+let num4 = 12;
+let mod = num3 % num4;
+console.log(mod); // Output: 4
+
+let max = Math.max(1, 2, 3, 4, 20); // Maximum value from the list
+console.log(max); // Output: 20
